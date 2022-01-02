2171d14d64328c6857599e0146668710a4ef55b3
janieleasy <69400926+janieleasy@users.noreply.github.com>
2022-01-03 02:04:09 +0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Navbar, Footer, Loader, Services, Transactions, Welcome } from './components';
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
